dcdde2294af307dbb714e15f50395bddff0a1ca9
garrettsimmons2772@gmail.com <garrettsimmons2772@gmail.com@bc7dcf84-fcbb-aadd-3b21-2e2720bc82b3>
2012-04-11 15:48:31 +0000

    A Team Theme Song

--- trunk/dannyworking/trunk/testdrive/protected/views/site/index.php
+++ trunk/dannyworking/trunk/testdrive/protected/views/site/index.php
@@ -3,7 +3,7 @@
 <h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 
 <p>Congratulations! You have successfully created your Yii application.</p>
-
+<p><object width="100%" height="25" class="hark_player"><param name="movie" value="http://cdn.hark.com/swfs/player_bar.swf?pid=flqpsjkbhv"/><param name="allowscriptaccess" value="always"/><param name="allownetworking" value="all"/><param name="wmode" value="transparent"/><embed src="http://cdn.hark.com/swfs/player_bar.swf?pid=flqpsjkbhv" type="application/x-shockwave-flash" allowscriptaccess="always" width="100%" height="25" wmode="transparent"></embed></object><br/><a href="http://www.hark.com/clips/flqpsjkbhv-the-a-team-theme-song-instrumental" style="font-size: 9px; color: #ddd;" title="Listen to The A-Team Theme Song (Instrumental) on Hark.com">The A-Team Theme Song (Instrumental)</a></p>
 <p>You may change the content of this page by modifying the following two files:</p>
 <ul>
 	<li>View file: <tt><?php echo __FILE__; ?></tt></li>
